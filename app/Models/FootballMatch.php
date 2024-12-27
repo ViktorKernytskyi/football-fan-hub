3c8362d0b23eb470a1a58e7feefc386e2a0cb1c0
ViktorKernytskyi <vihtor@ukr.net>
2024-12-27 22:49:23 +0200

    Created the matches table and the FootballMatch model

---
+++ app/Models/FootballMatch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class FootballMatch extends Model
+{
+    use HasFactory;
+
+    protected $table = 'matches'; // Name of the table in the database
+   // protected $fillable = ['team1', 'team2', 'match_date', 'stadium', 'score'];
+}
---
+++ database/migrations/2024_12_27_184756_create_matches_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->integer('id')->autoIncrement()->primary();
+            $table->string('team_home');
+            $table->string('team_away');
+            $table->dateTime('match_date');
+            $table->string('stadium');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
