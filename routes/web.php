00317fd6c228cf1dfa89a1c0cecaf8648c1292cd
ViktorKernytskyi <vihtor@ukr.net>
2025-01-01 23:48:00 +0200

    "Added route for match schedule page in `routes/web.php"

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MatchController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/matches', [MatchController::class, 'index'])->name('matches.index');
