5e60a813d3ee2d0975c87545edc24f03e30164be
ViktorKernytskyi <vihtor@ukr.net>
2025-01-02 20:49:59 +0200

    "Update Clients model: add $fillable and $hidden "

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+
+    // Fields that can be filled in bulk - Поля, які можуть бути заповнені масово.
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+    ];
+
+    // Fields that will be hidden when converting the model to JSON - Поля, які будуть приховані при перетворенні моделі у JSON.
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
