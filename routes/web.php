c436be0e92936516807c8916d92125dc5c91a186
ViktorKernytskyi <vihtor@ukr.net>
2025-01-02 22:57:45 +0200

    "Add CRUD routes for tickets."

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MatchController;
 
@@ -11,3 +12,9 @@ Route::get('/', function () {
 
 Route::get('/matches', [MatchController::class, 'index'])->name('matches.index');
 Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
+
+Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
+Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+Route::get('/tickets/{id}', [TicketController::class, 'show'])->name('tickets.show');
+Route::put('/tickets/{id}', [TicketController::class, 'update'])->name('tickets.update');
+Route::delete('/tickets/{id}', [TicketController::class, 'destroy'])->name('tickets.destroy');
