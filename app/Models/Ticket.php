0b0bdf3d405a15672170b3381cd51357ad8ce2bf
ViktorKernytskyi <vihtor@ukr.net>
2025-01-02 22:07:50 +0200

    "Define mass assignable fields and relationships with FootballMatch and Client models in Ticket model."

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    use HasFactory;
+
+    // Fields that can be filled in bulk - Поля, які можуть бути заповнені масово.
+    protected $fillable = [
+        'match_id',
+        'user_id',
+        'seat_number',
+        'price',
+    ];
+
+    // Determining the relationship with the match table - Визначення зв'язку з таблицею матчів
+    public function match()
+    {
+        return $this->belongsTo(FootballMatch::class);
+    }
+
+    // Defining a relationship with the users table - Визначення зв'язку з таблицею користувачів
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
 }
