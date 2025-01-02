92c522c305f7dd058336762da49d57427095cc38
ViktorKernytskyi <vihtor@ukr.net>
2025-01-02 20:54:08 +0200

    "Add ClientController to manage client-related functionality "

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Client;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class ClientController extends Controller
+{
+
+
+}
