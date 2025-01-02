b1236f1df624431331772a754518b960705d14b4
ViktorKernytskyi <vihtor@ukr.net>
2025-01-02 23:11:11 +0200

    "Created Match model with mass assignable fields and defined one-to-many relationship with Ticket model."

--- app/Models/FootballMatch.php
+++ app/Models/FootballMatch.php
@@ -11,4 +11,19 @@ class FootballMatch extends Model
 
     protected $table = 'matches'; // Name of the table in the database
    // protected $fillable = ['team1', 'team2', 'match_date', 'stadium', 'score'];
+
+    // Fields that can be filled in bulk - Поля, які можуть бути заповнені масово.
+    protected $fillable = [
+        'team_home',
+        'team_away',
+        'match_date',
+        'stadium',
+
+    ];
+    // Defining a relationship with the ticket table - Визначення зв'язку з таблицею квитків
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
+
 }
