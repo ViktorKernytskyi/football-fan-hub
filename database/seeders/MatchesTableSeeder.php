6e74637a62ac2cfab49d601a430de5b38252493a
ViktorKernytskyi <vihtor@ukr.net>
2025-01-04 22:22:22 +0200

    "Created a MatchesTableSeeder to insert sample data into a match table."

---
+++ database/seeders/MatchesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MatchesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('matches')->insert([
+            [
+                'team_home' => 'Arsenal',
+                'team_away' => 'Chelsi',
+                'match_date' => '2025-01-15',
+                'stadium' => 'Stadium A',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'team_home' => 'Liverpool',
+                'team_away' => 'Everton',
+                'match_date' => '2025-01-20',
+                'stadium' => 'Stadium B',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
