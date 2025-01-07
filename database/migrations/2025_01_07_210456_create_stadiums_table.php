8e72b56f3ccbdb16c4708352b41a6d73d4c4fd18
ViktorKernytskyi <vihtor@ukr.net>
2025-01-07 23:16:49 +0200

    "created the Stadium model and the stadiums table"

---
+++ database/migrations/2025_01_07_210456_create_stadiums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stadiums', function (Blueprint $table) {
+            $table->id(); // Unique stadium identifier - Унікальний ідентифікатор стадіону
+            $table->string('name'); // Stadium name - Назва стадіону
+            $table->integer('seat_count'); // Number of seats in the stadium - Кількість місць на стадіоні
+            $table->string('address'); // Stadium address - Адреса стадіону
+            $table->timestamps();
+
+
+            $table->softDeletes();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stadiums');
+    }
+};
