9e6538304a20f2d580ac16848812789c26177f04
ViktorKernytskyi <vihtor@ukr.net>
2025-01-07 23:30:42 +0200

    "Added a `location` column to the `stadiums` table to store the location (city)."

--- database/migrations/2025_01_07_210456_create_stadiums_table.php
+++ database/migrations/2025_01_07_210456_create_stadiums_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('stadiums', function (Blueprint $table) {
             $table->id(); // Unique stadium identifier - Унікальний ідентифікатор стадіону
             $table->string('name'); // Stadium name - Назва стадіону
+            $table->string('location'); // Місце розташування (місто)
             $table->integer('seat_count'); // Number of seats in the stadium - Кількість місць на стадіоні
             $table->string('address'); // Stadium address - Адреса стадіону
             $table->timestamps();
