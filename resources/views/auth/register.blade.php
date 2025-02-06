d43197227e42faa22797b9e2bd6d081ea25023c6
ViktorKernytskyi <vihtor@ukr.net>
2025-02-07 00:24:18 +0200

    "Fix Blade template: remove redundant HTML structure, update field names, and ensure correct asset loading"

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,10 +23,10 @@
                             @csrf
 
                             <div class="row mb-3">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                                <label for="client_name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                    <input id="client_name" type="text" class="form-control @error('client_name') is-invalid @enderror" name="client_name" value="{{ old('client_name') }}" required autocomplete="name" autofocus>
 
                                     @error('name')
                                     <span class="invalid-feedback" role="alert">
