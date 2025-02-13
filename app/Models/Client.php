e3d36d58836bdfed479465ed29cf144030c703cd
ViktorKernytskyi <vihtor@ukr.net>
2025-02-13 23:34:29 +0200

    "Create Client model for client authentication:  - Created Client model for working with users via guard 'client'.  - The model contains the necessary properties and methods for authentication."

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Client extends Model
+class Client extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
     protected $table = 'clients'; // Name of the table in the database
     // Fields that can be filled in bulk - Поля, які можуть бути заповнені масово.
     protected $fillable = [
