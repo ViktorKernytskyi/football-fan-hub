1f51d7728a20f2c927fc5831d5882dcfd5ff5d47
ViktorKernytskyi <vihtor@ukr.net>
2025-04-15 22:25:48 +0300

    "refactor(ticket): moved ticket purchase business logic to model   - replaced manual customer validation in purchase() method with Ticket::purchaseBy() call   - removed duplicate purchase() method  - used createForUser() method from Ticket model in buy() to create multiple tickets"

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FootballMatch;
 use App\Models\Ticket;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
@@ -13,19 +15,49 @@ class TicketController extends Controller
         return view('tickets.index', compact('tickets'));
     }
 
+    // Список квитків для авторизованого користувача
+    public function indexlist()
+    {
+        $tickets = Ticket::where('client_id', Auth::id())->with('match')->get();
+
+        return view('tickets.index', compact('tickets'));
+    }
+
     // Метод для покупки квитка
     public function purchase($id)
     {
-        $ticket = Ticket::findOrFail($id);
+        try {
+            $ticket = Ticket::findOrFail($id);
+            $ticket->purchaseBy(Auth::id());
 
-        if ($ticket->client_id !== null) {
-            return back()->withErrors(['error' => 'This ticket is no longer available.']);
+            return back()->with('success', 'Квиток успішно придбано!');
+        } catch (\Exception $e) {
+            return back()->withErrors(['error' => $e->getMessage()]);
         }
+    }
 
-        // Прив'язка квитка до клієнта (припускаємо, що є авторизований користувач)
-        $ticket->update(['client_id' => auth()->id()]);
+    // Купівля нових квитків
+    public function buy(Request $request)
+    {
+        $request->validate([
+            'match_id' => 'required|exists:matches,id',
+            'quantity' => 'required|integer|min:1',
+        ]);
 
-        return back()->with('success', 'Ticket purchased successfully!');
+       // $match = FootballMatch::find($request->match_id);
+        $pricePerTicket = 20; // Фіксована ціна за квиток
+
+        // Створення квитка
+        for ($i = 0; $i < $request->quantity; $i++) {
+            Ticket::create([
+                'client_id' => Auth::id(),
+                'match_id' => $request->match_id,
+                'quantity' => $request->quantity,
+                'total_price' => $pricePerTicket * $request->quantity,
+                'price' => $pricePerTicket,
+            ]);
+        }
+        return redirect()->route('tickets.index')->with('success', 'Квитки успішно придбані!');
     }
 
 }
