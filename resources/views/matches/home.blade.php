890f53490bd3ab2913c1ebb1fbbc25984f0d31df
ViktorKernytskyi <viktorkernytskyi@gmail.com>
2025-07-02 22:38:02 +0300

    init(linux): project adaptation for Linux, MySQL connection test after migration from Windows

--- resources/views/matches/home.blade.php
+++ resources/views/matches/home.blade.php
@@ -7,6 +7,16 @@
 </head>
 <body>
 <h1>Upcoming Matches</h1>
+<div class="col-sm-8">
+    <div class="menu pull-right">
+        <ul class="nav navbar-nav ms-auto">
+            <li><a href="{{ route('matches.home') }}"><i class="fa fa-crosshairs"></i>Upcoming Matches</a></li>
+            <li><a href="{{ route('matches.index') }}"><i class="fa fa-crosshair"></i> Match Schedule</a></li>
+            <li><a href="{{ route('news.index') }}"><i class="fa fa-crosshairs"></i> Latest Football News</a></li>
+            <li><a href="{{ route('tickets.store') }}"><i class="fa fa-crosshairs"></i> My Tickets</a></li>
+        </ul>
+    </div>
+</div>
 <ul>
     @foreach ($matches as $match)
         <li>
